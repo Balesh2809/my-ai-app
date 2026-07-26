9ecf4f5dd181e5d28e2175db2392b087e4716794
Balesh2809 <balesh6969@gmail.com>
2026-07-26 23:31:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ else:
         with st.spinner("Thinking..."):
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-2.0-flash",
                     contents=user_input,
                 )
                 st.success("Response:")
